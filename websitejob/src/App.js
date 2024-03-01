b10de68c41c895df6e95e2fe02eb280c460af9c8
AhmedMatssm <ahmed2003mouatassim@gmail.com>
2024-03-01 11:10:37 +0100

    This website updated

---
+++ websitejob/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='./updateA.png' className="App-logo" alt="logo" />
+        <p>
+        This site is updated
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
